6e95a7f692a51b63cbb17cab6e5a68ecf5cd11d8
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2022-05-29 15:43:46 -0300

    implementação de texto0.1

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let barra = document.getElementsByName("input#slide")
+function txt(){
+    let slide = document.getElementsByName("slide")
+    let tex = document.getElementById('texto')
 
+    if(slide[0].checked){
+        tex.innerHTML = "Strato é um guitarra legal"
+    }else if(slide[1].checked){
+        tex.innerHTML = "Telecaster é legal também"
+    }else if(slide[2].checked){
+        tex.innerHTML = "Les Paul é legal também"
+    }else if(slide[3].checked){
+        tex.innerHTML = "Soloist é legal também"
+    }else if(slide[4].checked){
+        tex.innerHTML = "Explorer é legal também"
+    }else if(slide[5].checked){
+        tex.innerHTML = "Flying V é legal também"
+    }else if(slide[6].checked){
+        tex.innerHTML = "Warlock é legal também"
+    }
+}
